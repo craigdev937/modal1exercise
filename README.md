5e30544795f027b46691d8287c9ffa032b73cbaf
Craig <indigomx9@gmail.com>
2022-07-17 19:47:41 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Portals
+
+Inital setup of the project.  Created with TypeScript, Webpack,  and React.
+
+
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, laudantium consectetur ex, perspiciatis animi commodi obcaecati delectus facilis ducimus illo, corporis ab veniam quidem iste. Ab corrupti cumque corporis deleniti.</p>
+        </React.Fragment>
+    );
+};
+
+
